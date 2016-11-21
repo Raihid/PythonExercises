59a562d2109bc3138ac8b13ad4c63cd564c1416e
Maciej Wołczyk <raihid888@gmail.com>
2016-11-21 21:32:33 +0100

    Added triangles class, fixed some issues

--- cw06/points.py
+++ cw06/points.py
@@ -1,53 +1,55 @@
+#!/usr/bin/python
+
 import unittest
 from math import sqrt
 class Point:
     """Klasa reprezentujaca punkty na plaszczyznie."""
 
-    def __init__(self, x=0, y=0):  
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
-    def __str__(self):         
+    def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
 
-    def __repr__(self):        
+    def __repr__(self):
         return "Point(" + str(self.x) + ", " + str(self.y) + ")"
 
-    def __eq__(self, other):   
+    def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
-    def __ne__(self, other):        
+    def __ne__(self, other):
         return not self == other
 
-    def __add__(self, other):  
+    def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other):   
+    def __sub__(self, other):
         return Point(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other):  
+    def __mul__(self, other):
         return self.x * other.x + self.y * other.y
 
-    def cross(self, other):         
+    def cross(self, other):
         return self.x * other.y - self.y * other.x
 
-    def length(self):          
+    def length(self):
         return sqrt(self.x*self.x + self.y*self.y)
 
 
 
 
 class TestPoint(unittest.TestCase):
-   
+
     def setUp(self):
-        self.p1 = Point(0, 0) 
-        self.p2 = Point(2, 0) 
-        self.p3 = Point(4.75, 4) 
+        self.p1 = Point(0, 0)
+        self.p2 = Point(2, 0)
+        self.p3 = Point(4.75, 4)
         self.p4 = Point(3.25, -2)
         self.p5 = Point(8, 2)
-        self.p6 = Point(11.25, 0) 
-        self.p7 = Point(-2, -2) 
-        self.p8 = Point(-4, 3.15) 
+        self.p6 = Point(11.25, 0)
+        self.p7 = Point(-2, -2)
+        self.p8 = Point(-4, 3.15)
         self.p9 = Point(-6, 1.15)
         self.p10 = Point(2.75, 4)
         self.p11 = Point(4.75, 4)
@@ -56,14 +58,14 @@ class TestPoint(unittest.TestCase):
 
     def test_print(self):
         self.assertEqual(str(self.p1), "(0, 0)")
-        self.assertEqual(str(self.p2), "(2, 0)") 
-        self.assertEqual(str(self.p3), "(4.75, 4)") 
+        self.assertEqual(str(self.p2), "(2, 0)")
+        self.assertEqual(str(self.p3), "(4.75, 4)")
         self.assertEqual(str(self.p4), "(3.25, -2)")
 
-    def test_repr(self):   
+    def test_repr(self):
         self.assertEqual(repr(self.p1), "Point(0, 0)")
-        self.assertEqual(repr(self.p2), "Point(2, 0)") 
-        self.assertEqual(repr(self.p3), "Point(4.75, 4)") 
+        self.assertEqual(repr(self.p2), "Point(2, 0)")
+        self.assertEqual(repr(self.p3), "Point(4.75, 4)")
         self.assertEqual(repr(self.p4), "Point(3.25, -2)")
 
     def test_add(self):
@@ -78,15 +80,15 @@ class TestPoint(unittest.TestCase):
         self.assertEqual(self.p5 - self.p4, self.p11)
         self.assertEqual(self.p9 - self.p8, self.p12)
 
-    def test_equals(self): 
+    def test_equals(self):
         self.assertTrue(self.p2 == self.p2)
         self.assertTrue(self.p1 == self.p1)
         self.assertTrue(self.p3 == Point(4.75, 4))
         self.assertTrue(self.p4 == Point(3.25, -2))
         self.assertFalse(self.p4 == self.p5)
         self.assertFalse(self.p6 == Point(11, 0))
-        
-    def test_not_equals(self): 
+
+    def test_not_equals(self):
         self.assertFalse(self.p2 != self.p2)
         self.assertFalse(self.p1 != self.p1)
         self.assertFalse(self.p3 != Point(4.75, 4))
@@ -99,17 +101,17 @@ class TestPoint(unittest.TestCase):
         self.assertEqual(self.p3 * self.p4, 7.4375)
         self.assertEqual(self.p5 * self.p6, 90)
 
-    def test_cross(self): 
+    def test_cross(self):
         self.assertEqual(self.p1.cross(self.p2), 0)
         self.assertEqual(self.p3.cross(self.p4), -22.5)
         self.assertEqual(self.p5.cross(self.p6), -22.5)
 
     def test_length(self):
-        self.assertEqual(self.p1.length(), 0) 
-        self.assertEqual(self.p2.length(), 2) 
-        self.assertEqual(self.p6.length(), 11.25) 
-        self.assertEqual(self.p13.length(), 5) 
-    
+        self.assertEqual(self.p1.length(), 0)
+        self.assertEqual(self.p2.length(), 2)
+        self.assertEqual(self.p6.length(), 11.25)
+        self.assertEqual(self.p13.length(), 5)
+
     def tearDown(self): pass
 
 if __name__ == '__main__':
--- cw06/rectangles.py
+++ cw06/rectangles.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from points import Point
 import unittest
 
@@ -5,37 +6,37 @@ import unittest
 class Rectangle:
     """Class representing a rectangle on a plane
        Assumptions: pt1 < pt2 (x1 < x2 and y1 < y2)
-       pt1 is bottom-left corner, pt2 is top-right corner 
+       pt1 is bottom-left corner, pt2 is top-right corner
     """
 
     def __init__(self, x1=0, y1=0, x2=0, y2=0):
         self.pt1 = Point(x1, y1)
         self.pt2 = Point(x2, y2)
 
-    def __str__(self):         
+    def __str__(self):
         return "["  + str(self.pt1) + ", " +  str(self.pt2) +  "]"
 
-    def __repr__(self):        
+    def __repr__(self):
         return "Rectangle(" +  str(self.pt1.x) +  ", " +  str(self.pt1.y) + \
                 ", " +  str(self.pt2.x) +  ", " +  str(self.pt2.y) +  ")"
 
-    def __eq__(self, other):   
+    def __eq__(self, other):
         return self.pt1 == other.pt1 and self.pt2 == other.pt2
 
-    def __ne__(self, other):        
+    def __ne__(self, other):
         return not self == other
 
-    def center(self):          
+    def center(self):
         return Point((self.pt1.x + self.pt2.x)/2, (self.pt1.y + self.pt2.y)/2)
 
-    def area(self):            
+    def area(self):
         return (self.pt2.x - self.pt1.x) * (self.pt2.y - self.pt1.y)
 
-    def move(self, x, y):      
+    def move(self, x, y):
         self.pt1 += Point(x, y)
         self.pt2 += Point(x, y)
 
-class TestRectangle(unittest.TestCase): 
+class TestRectangle(unittest.TestCase):
 
     def setUp(self):
         self.r1 = Rectangle(0, 0, 0, 0)
@@ -46,52 +47,52 @@ class TestRectangle(unittest.TestCase):
         self.r6 = Rectangle(-5, -5, 5, 5)
         self.r7 = Rectangle(2.25, 3.15, 8.5, 10.25)
 
-    def test_str(self): 
+    def test_str(self):
         self.assertEqual(str(self.r1), "[(0, 0), (0, 0)]")
         self.assertEqual(str(self.r2), "[(0, 0), (2, 2)]")
         self.assertEqual(str(self.r3), "[(1, 1), (4.5, 4.5)]")
         self.assertEqual(str(self.r4), "[(-2, 0), (4, 4)]")
 
-    def test_repr(self): 
+    def test_repr(self):
         self.assertEqual(repr(self.r1), "Rectangle(0, 0, 0, 0)")
         self.assertEqual(repr(self.r2), "Rectangle(0, 0, 2, 2)")
         self.assertEqual(repr(self.r3), "Rectangle(1, 1, 4.5, 4.5)")
         self.assertEqual(repr(self.r4), "Rectangle(-2, 0, 4, 4)")
 
-    def test_equals(self): 
+    def test_equals(self):
         self.assertTrue(self.r1 == self.r1)
         self.assertTrue(self.r2 == self.r2)
         self.assertTrue(self.r3 == Rectangle(1, 1, 4.5, 4.5))
         self.assertFalse(self.r1 == self.r2)
         self.assertFalse(self.r5 == Rectangle(-5, -5, -4, -4))
 
-    def test_not_equals(self): 
+    def test_not_equals(self):
         self.assertFalse(self.r1 != self.r1)
         self.assertFalse(self.r2 != self.r2)
         self.assertFalse(self.r3 != Rectangle(1, 1, 4.5, 4.5))
         self.assertTrue(self.r1 != self.r2)
         self.assertTrue(self.r5 != Rectangle(-5, -5, -4, -4))
 
-    def test_center(self): 
+    def test_center(self):
         self.assertEqual(self.r1.center(), Point(0, 0))
         self.assertEqual(self.r4.center(), Point(1, 2))
         self.assertEqual(self.r5.center(), Point(-4, -2))
         self.assertEqual(self.r6.center(), Point(0, 0))
 
-    def test_area(self): 
+    def test_area(self):
         self.assertEqual(self.r1.area(), 0)
         self.assertEqual(self.r2.area(), 4)
         self.assertEqual(self.r4.area(), 24)
         self.assertEqual(self.r7.area(), 44.375)
 
-    def test_move(self): 
+    def test_move(self):
         self.r1.move(3, 3)
         self.assertTrue(self.r1 == Rectangle(3, 3, 3, 3))
         self.r2.move(6, -1)
         self.assertTrue(self.r2 == Rectangle(6, -1, 8, 1))
         self.r7.move(8.5, 0)
         self.assertTrue(self.r7 == Rectangle(10.75, 3.15, 17, 10.25))
-    
+
     def tearDown(self): pass
 
 if __name__ == '__main__':
