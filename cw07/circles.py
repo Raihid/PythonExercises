3453ec32d8cb71e7e25e3cbe7bcf9759eea6d0b8
Maciej Wołczyk <raihid888@gmail.com>
2016-11-23 21:01:11 +0100

    Tests for circles

--- cw07/circles.py
+++ cw07/circles.py
@@ -3,7 +3,7 @@ from math import pi
 from points import Point
 
 class Circle:
-    """A class representing circles on a plane"""
+    """A class representing a circle on a plane"""
 
     def __init__(self, x=0, y=0, radius=1):
         if radius < 0:
@@ -12,7 +12,9 @@ class Circle:
         self.radius = radius
 
     def __repr__(self):       # "Circle(x, y, radius)"
-        return "Circle(" + str(self.x) + ", " + str(self.y) + ", " + str(radius) + ")"
+        return "Circle(" + str(self.pt.x) + ", " + str(self.pt.y) + ", " + str(self.radius) + ")"
+
+    __str__ = __repr__
 
     def __eq__(self, other):
         return self.pt == other.pt and self.radius == other.radius
@@ -21,20 +23,19 @@ class Circle:
         return not self == other
 
     def area(self):           # pole powierzchni
-        return pi*radius*radius
+        return pi*self.radius*self.radius
 
     def move(self, x, y):     # przesuniecie o (x, y)
-        self.x += x
-        self.y += y
+        self.pt += Point(x, y)
         
     def cover(self, other):   # todo: solid check
-        left = min(self.x - radius, other.x - radius)
-        bottom = min(self.y - radius,other.y - radius)
-        right = max(self.x + radius, other.x + radius)
-        top = max(self.y + radius, other.y + radius)
+        left = min(self.pt.x - self.radius, other.pt.x - other.radius)
+        bottom = min(self.pt.y - self.radius,other.pt.y - other.radius)
+        right = max(self.pt.x + self.radius, other.pt.x + other.radius)
+        top = max(self.pt.y + self.radius, other.pt.y + other.radius)
         center = Point((right - left)/2, (top - bottom)/2)
-        radius = max(center - left, center - bottom)
-        return Point(center.x, center.y, radius)
+        radius = max(center.x - left, center.y - bottom)
+        return Circle(center.x, center.y, radius)
         
 
 
@@ -44,12 +45,53 @@ import unittest
 
 class TestCircle(unittest.TestCase):
 
-    def setUp: pass
-    def test_repr: pass
-    def test_str: pass
-    def test_equals: pass
-    def test_not_equals: pass
-    def test_area: pass
-    def test_move: pass
-    def test_cover: pass
-    def tearDown: pass
+    def setUp(self):
+        self.c1 = Circle(0, 0, 5)
+        self.c2 = Circle(2, 2, 3)
+        self.c3 = Circle(0, 5, 3.5)
+        self.c4 = Circle(1, 2, 1.5)
+        self.c5 = Circle(3, 0.5, 6)
+        self.c6 = Circle(-2.5, -1.25, 5)
+        self.c7 = Circle(-3, 0, 2.2)
+        self.c8 = Circle(-8, 2, 0)
+
+    def test_repr(self):
+        self.assertEqual(repr(self.c1), "Circle(0, 0, 5)")
+        self.assertEqual(repr(self.c2), "Circle(2, 2, 3)")
+        self.assertEqual(repr(self.c3), "Circle(0, 5, 3.5)")
+        self.assertEqual(repr(self.c4), "Circle(1, 2, 1.5)")
+
+    def test_equals(self):
+        self.assertTrue(self.c1 == self.c1)
+        self.assertTrue(self.c2 == Circle(2.0, 2.0, 3.0))
+        self.assertTrue(self.c6 == Circle(-2.5, -1.25, 5))
+        self.assertFalse(self.c2 == self.c3)
+        self.assertFalse(self.c5 == Circle(0, 0, 5))
+
+    def test_not_equals(self):
+        self.assertFalse(self.c1 != self.c1)
+        self.assertFalse(self.c2 != Circle(2.0, 2.0, 3.0))
+        self.assertFalse(self.c6 != Circle(-2.5, -1.25, 5))
+        self.assertTrue(self.c2 != self.c3)
+        self.assertTrue(self.c5 != Circle(0, 0, 5))
+
+    def test_area(self):
+        self.assertEqual(self.c1.area(), 5*5*pi) 
+        self.assertEqual(self.c8.area(), 0) 
+        self.assertEqual(self.c3.area(), 3.5*3.5*pi) 
+
+    def test_move(self):
+        self.c1.move(3, 3)
+        self.assertTrue(self.c1 == Circle(3, 3, 5))
+        self.c2.move(6, -1)
+        self.assertTrue(self.c2 == Circle(8, 1, 3))
+        self.c7.move(8.5, 0)
+        self.assertTrue(self.c7 == Circle(5.5, 0, 2.2))
+        
+    def test_cover(self): 
+        self.assertEqual(self.c1.cover(self.c8), self.c1)    
+
+    def tearDown(self): pass
+
+if __name__ == '__main__':
+    unittest.main()
