255f7a2a31cc4469661fde9cc2e23c8eaed00495
Maciej Wołczyk <raihid888@gmail.com>
2016-11-24 16:32:17 +0100

    Small changes with circles

--- cw07/circles.py
+++ cw07/circles.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from math import pi
+from math import sqrt
 from points import Point
 
 class Circle:
@@ -27,16 +28,19 @@ class Circle:
 
     def move(self, x, y):     # przesuniecie o (x, y)
         self.pt += Point(x, y)
-        
-    def cover(self, other):   # todo: solid check
-        left = min(self.pt.x - self.radius, other.pt.x - other.radius)
-        bottom = min(self.pt.y - self.radius,other.pt.y - other.radius)
-        right = max(self.pt.x + self.radius, other.pt.x + other.radius)
-        top = max(self.pt.y + self.radius, other.pt.y + other.radius)
-        center = Point((right - left)/2, (top - bottom)/2)
-        radius = max(center.x - left, center.y - bottom)
-        return Circle(center.x, center.y, radius)
-        
+
+    def cover(self, other):   # todo: prawdopodobnie działa, ale testy
+        c1, c2 = sorted([self, other], key = lambda item: item.radius)
+
+        distance = sqrt((c1.pt.x - c2.pt.x) ** 2 + (c1.pt.y - c2.pt.y) ** 2)
+        if(distance + c1.radius <= c2.radius):
+            return c2
+        final_radius = (distance + c1.radius + c2.radius)/2.0
+        alpha = 1.0/2.0 + (c2.radius - c1.radius)/(2.0*distance)
+        center = Point((1-alpha)*c1.pt.x + alpha*c2.pt.x,
+                       (1-alpha)*c1.pt.y + alpha*c2.pt.y)
+        return Circle(center.x, center.y, final_radius)
+
 
 
 # Kod testujacy modul.
@@ -54,6 +58,8 @@ class TestCircle(unittest.TestCase):
         self.c6 = Circle(-2.5, -1.25, 5)
         self.c7 = Circle(-3, 0, 2.2)
         self.c8 = Circle(-8, 2, 0)
+        self.c9 = Circle(-2, -2, 0)
+        self.c10 = Circle(10, 10, 5)
 
     def test_repr(self):
         self.assertEqual(repr(self.c1), "Circle(0, 0, 5)")
@@ -76,9 +82,9 @@ class TestCircle(unittest.TestCase):
         self.assertTrue(self.c5 != Circle(0, 0, 5))
 
     def test_area(self):
-        self.assertEqual(self.c1.area(), 5*5*pi) 
-        self.assertEqual(self.c8.area(), 0) 
-        self.assertEqual(self.c3.area(), 3.5*3.5*pi) 
+        self.assertEqual(self.c1.area(), 5*5*pi)
+        self.assertEqual(self.c8.area(), 0)
+        self.assertEqual(self.c3.area(), 3.5*3.5*pi)
 
     def test_move(self):
         self.c1.move(3, 3)
@@ -87,9 +93,13 @@ class TestCircle(unittest.TestCase):
         self.assertTrue(self.c2 == Circle(8, 1, 3))
         self.c7.move(8.5, 0)
         self.assertTrue(self.c7 == Circle(5.5, 0, 2.2))
-        
-    def test_cover(self): 
-        self.assertEqual(self.c1.cover(self.c8), self.c1)    
+
+    def test_cover(self):
+        self.assertEqual(self.c1.cover(self.c9), self.c1)
+        self.assertEqual(self.c1.cover(self.c3), Circle(0, 1.75, 6.75))
+        self.assertEqual(self.c1.cover(self.c10), Circle(3, 0.5, 6))
+
+
 
     def tearDown(self): pass
 
