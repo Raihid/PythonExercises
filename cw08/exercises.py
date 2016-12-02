485c2cbcb7d62ada891b1c5b1a43f99c880c2e45
Maciej Wołczyk <raihid888@gmail.com>
2016-12-02 02:26:45 +0100

    Fixed most exercised, stylised by PEP8

--- cw08/exercises.py
+++ cw08/exercises.py
@@ -6,21 +6,38 @@ from math import sqrt
 # ----- Exercise 1 -----
 def solve1(a, b, c):
     """Solving linear equation a x + b y + c = 0."""
-    b = -(b*1.0/a)
-    c = -(c*1.0/a)
-    solution = "x = " + str(b) + "*y"
-    if c != 0:
-        solution += " + " if c > 0 else " - "
-        solution += str(abs(c))
-    print(solution)
+    if a == 0 and b == 0:
+        if c == 0:
+            print("Equation is satisified by all real numbers (x, y)")
+        else:
+            print("Impossible equation " + str(c) + " == 0 ")
+    elif b == 0:
+        x = -c/a
+        print("x = " + str(x))
+    elif a == 0:
+        y = -c/b
+        print("x = " + str(y))
+    else:
+        coeff_b = -(b*1.0/a)
+        coeff_c = -(c*1.0/a)
+        solution = "x = " + str(coeff_b) + "*y"
+        if c != 0:
+            solution += " + " if c > 0 else " - "
+            solution += str(abs(coeff_c))
+        print(solution)
 
 
 solve1(1, 1, 0)
+solve1(2, 2, 0)
 
 
 # ----- Exercise 2 -----
 def solve2(a, b, c):
     delta = sqrt(b*b - 4*a*c)
+    if a == 0:
+        solve1(0, b, c)
+        return
+
     if delta == 0:
         return -b/(2.0*a)
     elif delta > 0:
@@ -29,9 +46,10 @@ def solve2(a, b, c):
         return None
 
 
+print(solve2(0, 5, 3))
 print(solve2(1, 5, 3))
 
-
+# ----- Exercise 3 -----
 def calc_pi(n=100):
     hit = 0
     for i in range(0, n):
@@ -44,17 +62,19 @@ def calc_pi(n=100):
 
 print(calc_pi())
 print(calc_pi(10000))
-            
-    
+
+
+# ----- Exercise 4 -----
 def heron(a, b, c):
-    if a > b + c or b > a + c or c > a + b :
+    if a > b + c or b > a + c or c > a + b:
         raise ValueError
     s = (a+b+c)/2.0
     return sqrt(s*(s-a)*(s-b)*(s-c))
 
-print(heron(6,6,6))
+print(heron(6, 6, 6))
 
 
+# ----- Exercise 5 -----
 def ackermann(n, p):
     """Funkcja Ackermanna, n i p to liczby naturalne.
     Zachodzi A(0, p) = 1, A(n, 1) = 2n, A(n, 2) = 2 ** n,
@@ -70,26 +90,32 @@ def ackermann(n, p):
     if p >= 0 and n >= 1:
         return ackermann(ackermann(n-1, p), p-1)
 
+
 def examine_ackermann():
     results = {}
-    for p in range(1,5):
+    for p in range(1, 3):
         results[p] = {}
         for n in range(1, 5):
             results[p][n] = ackermann(n, p)
     print(results)
 
-# examine_ackermann()
 
+print("Ackermann!!!")
+examine_ackermann()
+
+
+# ----- Exercise 6 -----
 def dynamic_P(n, m):
     P = {i: {0: 0} for i in range(1, n+1)}
     P[0] = {0: 0}
     for j in range(1, m+1):
         P[0][j] = 1.0
 
-    for i in range(1, n+1): 
+    for i in range(1, n+1):
         for j in range(1, m+1):
             P[i][j] = 0.5*(P[i-1][j] + P[i][j-1])
     return P[n][m]
+
 
 def recursive_P(i, j):
     if i == 0 and j == 0:
@@ -100,6 +126,7 @@ def recursive_P(i, j):
         return 1.0
     else:
         return 0.5*(recursive_P(i-1, j) + recursive_P(i, j-1))
-print(dynamic_P(5,11))
-print(recursive_P(5,11))
 
+
+print(dynamic_P(5, 11))
+print(recursive_P(5, 11))
